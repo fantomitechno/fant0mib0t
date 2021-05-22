2b7f69b54f8c08dc1b024769e69f4ec9ae76be47
fantomitechno <fantomitechno@gmail.com>
2021-05-22 09:30:19 +0200

    functions modifications

--- src/functions/logging.ts
+++ src/functions/logging.ts
@@ -1,12 +1,12 @@
-import { Message, NewsChannel, TextChannel } from "discord.js";
+import { Guild } from "discord.js";
 import { BetterEmbed } from "discord.js-better-embed/dist";
 
 
-export const sendToModLogs = (message: Message, content: string, type: string) => {
+export const sendToModLogs = (guild: Guild | null, content: string, type: string) => {
     let logEmbed = new BetterEmbed({
         title: `New ${type}`,
         description: content
     })
-    const channel: any = message.guild?.channels.cache.find(c => (c.name.includes("mod-logs") || c.name.includes("logs-mod")) && (c.type === "text" || c.type === "news"))
+    const channel: any = guild?.channels.cache.find(c => (c.name.includes("mod-logs") || c.name.includes("logs-mod")) && (c.type === "text" || c.type === "news"))
     channel?.send(logEmbed)
 }
