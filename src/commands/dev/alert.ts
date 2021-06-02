75fbded25e693060b40de9da01335c27bea99b95
fantomitechno <fantomitechno@gmail.com>
2021-06-02 17:02:09 +0200

    Dev commands (aka PANIC)

---
+++ src/commands/dev/alert.ts
@@ -0,0 +1,32 @@
+import { Command, CommandHandler, Tag } from 'advanced-command-handler'
+import { Context } from '../../class/Context';
+
+
+export default new Command(
+	{
+		name: 'alert',
+		description: "DANGER ZONE, DON'T TOUCH ANYTHING",
+        tags: [Tag.ownerOnly],
+		aliases: ['panic']
+	},
+	async (handler: typeof CommandHandler, ctx: Context) => {
+        ctx.send(`<a:red_alert:849643442514296843> You're entering the danger zone, the command you runned is WORTHER than the shutdown :\nThis command have to be used in an absolute need !\n\nWhat will appen after you touch the red button :\nThe bot will leave all his guild and DM the owner of the situation\n\nExact message :\`\`\` \`\`\`\n<a:red_alert:849643442514296843> Hi, I'm one of your server ({guild.name}) bots. My token have been leaked and my owners decided to enter the danger zone : I have to leave your server so you can't be raided by me. For more informations, go on my Discord server : <http://discord.gg/x9BMZ6z>\`\`\` \`\`\``).then(async m => {
+            await m.react('🔴')
+            await m.react('🚫')
+            const col = m.createReactionCollector((reaction, user) => ['🔴','🚫'].includes(reaction.emoji.name) && user.id == ctx.author.id, {time: 30000})
+            col.on('collect', (r, _) => {
+                col.stop()
+                m.delete()
+                if (r.emoji.name === '🔴') {
+                    ctx.send(`You pressed the red button, good bye master <:salute:849646594759721041>`)
+                    handler.client?.guilds.cache.map(g => {
+                        g.owner?.send(`<a:red_alert:849643442514296843> Hi, I'm on one of your servers (${g.name}). My token have been leaked and my owners decided to enter the danger zone : I have to leave your server so you can't be raided by me. For more informations, go on my Discord server : <http://discord.gg/x9BMZ6z>`).catch(e => e).then(_ => {
+                            g.leave()
+                        })
+                    })
+                }
+                else ctx.send(`Canceled, NEVER DO THAT AGAIN`)
+            })
+        })
+    }
+);
---
+++ src/commands/dev/reset.ts
@@ -0,0 +1,30 @@
+import { Command, CommandHandler, Tag } from 'advanced-command-handler'
+import { MysqlError } from 'mysql';
+import { Context } from '../../class/Context';
+import { query } from '../../functions/db';
+import { getWebhook } from '../../functions/get';
+import { SConfig } from '../../type/Database';
+
+
+export default new Command(
+	{
+		name: 'reset',
+		description: "Reset the current server",
+        tags: [Tag.ownerOnly, Tag.guildOnly]
+	},
+	async (handler: typeof CommandHandler, ctx: Context) => {
+        if (!ctx.guild) return
+        let text = `Reset : `
+        getWebhook(ctx.guild)
+        query("SELECT * FROM config WHERE guild = '"+ctx.guild.id+"'", async (err: MysqlError, res: SConfig[]) => {
+            if (!res.length) {
+                query(`INSERT INTO config (guild, config, webhook) VALUES ("${ctx.guild?.id}", '{"automod":{"antilink": true, "uppercase":true, "spam":true, "dupplicated":true}, "antilinkBypass": "", "linkPreview":true}')`)
+                text += `\nCreated a sconfig`
+            } else {
+                query(`DELETE FROM config WHERE guild = "${ctx.guild?.id}"`)
+                query(`INSERT INTO config (guild, config, webhook) VALUES ("${ctx.guild?.id}", '{"automod":{"antilink": true, "uppercase":true, "spam":true, "dupplicated":true}, "antilinkBypass": "", "linkPreview":true}')`)
+                text += `\nReseted the sconfig`
+            }
+        })
+	}
+);
